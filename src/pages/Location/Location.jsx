68ddd15668550ed0f536d6d40dfdf9fd679fe03a
Hira <hirahaiderali1975@gmail.com>
2024-09-25 12:31:58 +0500

    location page updated

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -1,30 +1,66 @@
 import React, { useState } from 'react';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import { TbCurrentLocation } from "react-icons/tb";
+import { IoIosArrowBack } from "react-icons/io";
+import { LuCalendarDays } from "react-icons/lu";
 import 'leaflet/dist/leaflet.css';
 import './Location.css';
 
 export default function Location() {
   const [coordinates, setCoordinates] = useState([47.6062, -122.3321]);
+  const [location, setLocation] = useState('');
 
-  const handleSelect = (location) => {
-    const lat = location.geometry.location.lat();
-    const lng = location.geometry.location.lng();
-    setCoordinates([lat, lng]);
+  const SetViewOnChange = ({ coords }) => {
+    const map = useMap();
+    map.setView(coords, 13);
+    return null;
   };
 
-  const apiKey = import.meta.env.VITE_API_KEY;
+  const fetchCoordinates = async () => {
+    const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${location}`);
+    const data = await response.json();
+
+    if (data && data.length > 0) {
+      const lat = parseFloat(data[0].lat);
+      const lon = parseFloat(data[0].lon);
+      setCoordinates([lat, lon]);
+    } else {
+      console.log('Location not found');
+    }
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    fetchCoordinates();
+  };
 
   return (
-    <div className='location-page'>
-      <div className="search-bar">
-        <GooglePlacesAutocomplete
-          apiKey={apiKey}
-          selectProps={{
-            onChange: handleSelect,
-            placeholder: 'Find Photographers'
-          }}
-        />
+    <div className="location-page">
+      <div className="search-container flex">
+        <div onChange={handleChange} className="search flex">
+          <div className='input--group flex'>
+            <IoIosArrowBack />
+            <input
+              type="text"
+              placeholder="Find for Photgraphers"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div className='loc-div'>
+            <TbCurrentLocation />
+          </div>
+        </div>
+        <div className="date-container flex">
+          <div className='date-group flex'>
+            <input type="text" placeholder='From' />
+            <LuCalendarDays />
+          </div>
+          <div className='date-group flex'>
+            <input type="text" placeholder='To' />
+            <LuCalendarDays />
+          </div>
+        </div>
       </div>
 
       <MapContainer center={coordinates} zoom={13} style={{ height: "500px", width: "100%" }}>
@@ -32,8 +68,9 @@ export default function Location() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={coordinates}>
-        </Marker>
+        <Marker position={coordinates}></Marker>
+
+        <SetViewOnChange coords={coordinates} />
       </MapContainer>
     </div>
   );
