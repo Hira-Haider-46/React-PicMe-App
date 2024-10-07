9216591b2f203f9e661938f965a2ea5479c898e8
Hira <hirahaiderali1975@gmail.com>
2024-10-07 12:50:22 +0500

    profile page updated

--- src/pages/PhotographerSide/ProfilePage/ProfilePage.jsx
+++ src/pages/PhotographerSide/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { FiUpload } from "react-icons/fi";
+import Button from '../../../commonComponents/Button';
 import './ProfilePage.css';
 
 export default function ProfilePage() {
   return (
-    <div>
-      
+    <div className='profile-page'>
+      <h2>Create Your Profile</h2>
+      <p>Update your details in the form provided.</p>
+      <form>
+        <div className="input-field">
+          <input type="text" placeholder='Full Name' />
+        </div>
+        <div className="input-field">
+          <select>
+            <option value="" disabled selected>Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div className="input-field">
+          <input type="text" placeholder='Address' />
+        </div>
+        <div className="input-field">
+          <input type="text" placeholder='Add Custom Photographer Type' />
+        </div>
+        <div className="input-field">
+          <select>
+            <option value="" disabled selected>Add Photographer Type</option>
+            <option value="">No options available</option>
+          </select>
+        </div>
+        <div className="file-upload">
+          <label htmlFor="id-card-upload" className="upload-box">
+            <img src="" alt="Upload ID Card" />
+            <p>Upload ID Card<br />Upload only in png, jpeg.</p>
+          </label>
+          <input id="id-card-upload" type="file" accept="image/png, image/jpeg" />
+        </div>
+        <Button text='SUBMIT' variant='fill'/>
+      </form>
     </div>
-  )
+  );
 }
