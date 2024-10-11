93baaa9872c1f8fc974aee03ea71ec436abd1c16
Hira <hirahaiderali1975@gmail.com>
2024-10-11 09:16:08 +0500

    loader added in create pkg

--- src/pages/PhotographerSide/CreatePackage/CreatePackage.jsx
+++ src/pages/PhotographerSide/CreatePackage/CreatePackage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { LuLoader2 } from "react-icons/lu";
 import { getApiWithAuth } from '../../../apis/index';
 import { SHOW_PACKAGE } from '../../../apis/apiUrls';
 import Button from '../../../commonComponents/Button';
@@ -9,16 +10,18 @@ import './CreatePackage.css';
 export default function CreatePackage() {
 
     const [packages, setPackages] = useState(null);
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
 
     const fetchPackages = async () => {
+        setLoading(true);
         const res = await getApiWithAuth(SHOW_PACKAGE);
         if (res.success) {
-            console.log('packages', res.data.data);
             setPackages(res.data.data);
         } else {
             console.error(res.data);
         }
+        setLoading(false);
     }
 
     const handleCreatPackage = () => {
@@ -39,13 +42,14 @@ export default function CreatePackage() {
                         <PackageCard
                             key={index}
                             pkg={pkg}
-                            refreshPackages={fetchPackages} 
+                            refreshPackages={fetchPackages}
                         />
                     ))}
                 </div>
-            ) : (
-                <p className='select-msg' style={{ marginTop: '2em' }}>No packages available</p>
-            )}
+            ) : <>
+                    {loading ? <LuLoader2 className="loader" /> : <p className='select-msg' style={{ marginTop: '2em' }}>No packages available</p>}
+            </>
+            }
             <div className='btn' onClick={handleCreatPackage}>
                 <Button text='CREATE NEW PACKAGE' variant='empty' />
             </div>
