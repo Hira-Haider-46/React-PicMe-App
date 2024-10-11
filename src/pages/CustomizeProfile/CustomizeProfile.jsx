c03eb02f08b578feb04b8da1c245684dca56b98d
Hira <hirahaiderali1975@gmail.com>
2024-10-11 21:25:05 +0500

    customize profile page updated

--- src/pages/CustomizeProfile/CustomizeProfile.jsx
+++ src/pages/CustomizeProfile/CustomizeProfile.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Button from '../../commonComponents/Button';
 import Input from '../../commonComponents/Input';
-import profile from '../../assets/images/profileImg.png';
+import profile from '../../assets/images/ProfileImg.png';
 import editProfile from '../../assets/images/editProfile.png';
 import { EDIT_PROFILE } from '../../apis/apiUrls';
 import { patchApiWithAuth } from '../../apis/index';
