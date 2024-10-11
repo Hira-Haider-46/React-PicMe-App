594e5cf3170365707436ad233c3a8bb47bc00281
Hira <hirahaiderali1975@gmail.com>
2024-10-11 13:33:20 +0500

    customer form updated

--- src/pages/CustomizeProfile/CustomerForm/CustomerForm.jsx
+++ src/pages/CustomizeProfile/CustomerForm/CustomerForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import Button from '../../../commonComponents/Button';
 import { EDIT_PROFILE } from '../../../apis/apiUrls';
 import { patchApiWithAuth } from '../../../apis/index';
@@ -17,31 +18,65 @@ export default function CustomerForm({ email, id }) {
         confirmPassword: '',
     });
 
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+    const nameRegex = /^[a-zA-Z0-9_ ]*$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    const validateField = (name, value) => {
+        let error = '';
+
+        switch (name) {
+            case 'fullName':
+                if (!nameRegex.test(value)) {
+                    error = 'Name can only contain letters, numbers and underscores';
+                }
+                break;
+            case 'password':
+                if (!passwordRegex.test(value)) {
+                    error = 'Password must contain at least 8 characters, including uppercase, lowercase, numbers, and special characters';
+                }
+                break;
+            case 'confirmPassword':
+                if (value !== formValues.password) {
+                    error = 'Passwords do not match';
+                }
+                break;
+            default:
+                break;
+        }
+
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            [name]: error,
+        }));
+    };
+
     const handleInputChange = (e) => {
+        const { name, value } = e.target;
         setFormValues({
             ...formValues,
-            [e.target.name]: e.target.value,
+            [name]: value,
         });
+        validateField(name, value);
     };
 
     const validateForm = () => {
-        const errors = {};
-        const nameRegex = /^[a-zA-Z0-9_ ]*$/;
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
+        const validationErrors = {};
         if (!nameRegex.test(formValues.fullName)) {
-            errors.fullName = 'Name can only contain letters, numbers, underscores, and spaces';
+            validationErrors.fullName = 'Name can only contain letters, numbers and underscores';
         }
 
         if (!passwordRegex.test(formValues.password)) {
-            errors.password = 'Password must contain at least 8 characters, including uppercase, lowercase, numbers, and special characters';
+            validationErrors.password = 'Password must have atleast 8 characters, including uppercase, lowercase, numbers, and special characters';
         }
 
         if (formValues.password !== formValues.confirmPassword) {
-            errors.confirmPassword = 'Passwords do not match';
+            validationErrors.confirmPassword = 'Passwords do not match';
         }
 
-        return errors;
+        return validationErrors;
     };
 
     const handleEditProfile = (e) => {
@@ -55,15 +90,27 @@ export default function CustomerForm({ email, id }) {
     };
 
     const editProfile = async () => {
-        const res = await patchApiWithAuth(`${EDIT_PROFILE}${id}`, {
-            fullName: formValues.fullName,
-            password: formValues.password,
-        });
+        const payload = {
+            user: {
+                fullName: formValues.fullName,
+                password: formValues.password
+                // profile_image: formValues.profile_image 
+            }
+        };
+        const res = await patchApiWithAuth(`${EDIT_PROFILE}${id}`, {payload});
         if (res.success) {
             console.log('Profile updated successfully');
         } else {
             console.error(res.data);
         }
+    };
+
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const toggleShowConfirmPassword = () => {
+        setShowConfirmPassword(!showConfirmPassword);
     };
 
     return (
@@ -79,14 +126,18 @@ export default function CustomerForm({ email, id }) {
                 />
             </div>
             <div className='form-group'>
-                <input
-                    type="password"
-                    name="confirmPassword"
-                    placeholder="Confirm Password"
-                    className='form-input'
-                    value={formValues.confirmPassword}
-                    onChange={handleInputChange}
-                />
+                <div className='input-group flex pass'>
+                    <input
+                        type={showConfirmPassword ? "text" : "password"}
+                        name="confirmPassword"
+                        placeholder="Confirm Password"
+                        value={formValues.confirmPassword}
+                        onChange={handleInputChange}
+                    />
+                    <div onClick={toggleShowConfirmPassword}>
+                        {showConfirmPassword ? <FaEye /> : <FaEyeSlash />}
+                    </div>
+                </div>
                 {errors.confirmPassword && <span className='error-message'>{errors.confirmPassword}</span>}
             </div>
             <div className='form-group'>
@@ -101,14 +152,18 @@ export default function CustomerForm({ email, id }) {
                 {errors.fullName && <span className='error-message'>{errors.fullName}</span>}
             </div>
             <div className='form-group'>
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Your password"
-                    className='form-input'
-                    value={formValues.password}
-                    onChange={handleInputChange}
-                />
+                <div className='input-group flex pass'>
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        placeholder="Your password"
+                        value={formValues.password}
+                        onChange={handleInputChange}
+                    />
+                    <div onClick={toggleShowPassword}>
+                        {showPassword ? <FaEye /> : <FaEyeSlash />}
+                    </div>
+                </div>
                 {errors.password && <span className='error-message'>{errors.password}</span>}
             </div>
             <div className='buttons flex'>
